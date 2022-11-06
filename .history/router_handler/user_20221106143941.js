f9f2d9a90ff0c5547258dc526f86378b8c7124aa
隔壁班的白芷 <1069936623@qq.com>
2022-11-06 14:41:40 +0800

    检测注册合法性

--- .history/router_handler/user_20221106143941.js
+++ .history/router_handler/user_20221106143941.js
@@ -1,9 +1,6 @@
 exports.regUser = (req, res) => {
     //获取用户注册信息
     const userinfo = req.body
-    if (!userinfo.username || !userinfo.password) {
-        return res.send('用户名或密码不能为空')
-    }
     console.log(userinfo)
     res.send('reg ok')
 }
